9633f282ddf9a8d8d5d5eb5e4978798d3798d5fa
char-c3 <work238fnct@gmail.com>
2019-07-14 13:37:43 +0000

    tutorial 1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #! /bin/bash -xe
 
-cp -ap map-test /usr/share/nginx/html/
-sed -i "s/<placeholder:google-maps-api-key>/$GOOGLE_MAPS_API_KEY/g" /usr/share/nginx/html/map-test/index.html
+HTMLDIR=/usr/share/nginx/html
+SRCDIR=map-test
+PRJDIR=$HTMLDIR/$SRCDIR
+
+cp -ap $SRCDIR $HTMLDIR
+for htmlfile in $(ls $PRJDIR/); do
+  sed -i "s/<placeholder:google-maps-api-key>/$GOOGLE_MAPS_API_KEY/g" $PRJDIR/$htmlfile
+done
